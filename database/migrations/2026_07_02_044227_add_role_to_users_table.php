4fa30e9be7d483c7e36e3c5dffb7859231419ffa
Egista Fatmawati <egistafatmawati72@gmail.com>
2026-07-02 14:22:38 +0700

    Add admin middleware

---
+++ database/migrations/2026_07_02_044227_add_role_to_users_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', ['admin', 'pengguna'])
+                  ->default('pengguna')
+                  ->after('email');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+};
