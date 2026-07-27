bcdb24b288b3dc9e1348cc236da0ac96551dc06e
OktaSFT <safitriokta131@gmail.com>
2026-07-28 06:31:18 +0700

    Trust Render HTTPS proxies to fix logout redirect issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\PreventBackHistory::class,
         ]);
